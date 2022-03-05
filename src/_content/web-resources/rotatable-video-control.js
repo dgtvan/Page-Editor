57a0071e0f05bac4732c671fd039d3551e021221
Van <20492454+VanDng@users.noreply.github.com>
2022-03-05 16:18:50 +0700

    impl

--- src/_content/web-resources/rotatable-video-control.js
+++ src/_content/web-resources/rotatable-video-control.js
@@ -2,23 +2,32 @@ PGE.HttpRequest.Send({
     url: 'https://raw.githubusercontent.com/VanDng/RotatableVideoControl/main/script.js'
 }).then(response => {
 
-    let scriptTag = document.createElement('script');
-    scriptTag.type = 'text/javascript';
-    scriptTag.async = true;
+    if (response.blocked) {
 
-    let videoControlScript = response.responseText + 'setInterval(function() { InitializeVideo(); }, 1000);';
+        console.log('[PGE] Could not initialize rotatable video control.');
 
-    let scriptProcName = 'pge_script_video_control';
-    let scriptContent = '';
-    scriptContent += 'if (/complete|interactive|loaded/.test(document.readyState)) {';
-    scriptContent += scriptProcName + '();';
-    scriptContent += '} else {';
-    scriptContent += 'document.addEventListener(\'DOMContentLoaded\', ' + scriptProcName + ', false);} ';
-    scriptContent += 'async function ' + scriptProcName + '() {' + videoControlScript + '}';
+    } else {
 
-    //let scriptContent = `window.addEventListener('DOMContentLoaded', (event) => {` + videoControlScript + `});`;
-    
-    scriptTag.innerHTML = scriptContent;
-    
-    document.head.appendChild(scriptTag);
+        let scriptTag = document.createElement('script');
+        scriptTag.type = 'text/javascript';
+        scriptTag.async = true;
+
+        let videoControlScript = response.responseText + ' setInterval(function() { InitializeVideo(); }, 1000);';
+
+        let scriptProcName = 'pge_script_video_control';
+        let scriptContent = '';
+        scriptContent += 'if (/complete|interactive|loaded/.test(document.readyState)) {';
+        scriptContent += scriptProcName + '();';
+        scriptContent += '} else {';
+        scriptContent += 'document.addEventListener(\'DOMContentLoaded\', ' + scriptProcName + ', false);} ';
+        scriptContent += 'async function ' + scriptProcName + '() {' + videoControlScript + '}';
+
+        //let scriptContent = `window.addEventListener('DOMContentLoaded', (event) => {` + videoControlScript + `});`;
+        
+        scriptTag.innerHTML = scriptContent;
+        
+        document.head.appendChild(scriptTag);
+        
+    }
+
 });
