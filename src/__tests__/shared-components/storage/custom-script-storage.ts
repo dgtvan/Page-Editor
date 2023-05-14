949c738d5350f6668b50023b614e97e3e14eb8f9
AzureAD\VanDangThai <van.dang@aswhiteglobal.com>
2023-05-14 17:51:02 +0700

    remove pupeteer, re-install sinon-chrome. give up on unit testing the service worker.

--- src/__tests__/shared-components/storage/custom-script-storage.ts
+++ src/__tests__/shared-components/storage/custom-script-storage.ts
@@ -1,36 +1,16 @@
-import puppeteer, { Browser } from 'puppeteer';
-import path from 'path';
+import chrome from 'sinon-chrome';
 import { CustomScriptStorage } from "../../../4-shared-components/storage/custom-script-storage"; 
 
-const pathToExtension =path.join(__dirname, '..', '..', '..', 'dist');
-
-const puppeteerArgs = [
-    `--disable-extensions-except=${pathToExtension}`,
-    `--load-extension=${pathToExtension}`,
-    '--show-component-extension-options',
-];
-  
 describe('okay la', () => {
-    let page, browser: Browser;
-
     beforeAll(async () => {
-        browser = await puppeteer.launch({
-            headless: false,
-            slowMo: 250,
-            devtools: false,
-            args: puppeteerArgs
-        });
-
-        // Creates a new tab
-        page = await browser.newPage();
-
-        // navigates to some specific page
-        //await page.goto('https://google.com');
+        // TODO: Wait for sinon-chorome to support the latest chrome extension API.
+        // https://github.com/acvetkov/sinon-chrome/issues/112
+        
+        //global.chrome = chrome
     });
 
     afterAll(async () => {
-        // Tear down the browser
-        await browser.close();
+        chrome.flush();
     })
 
     it('get a non-existing key', async () => {
